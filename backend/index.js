263a0308741b82328e82c92459cca1edf6bf9352
puntikuraakhilesh <akhilesh.p1211@gmail.com>
2024-12-19 10:58:55 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
+
+app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+
+
+app.use("/api/auth", authRoute);
+
+
+
+
+const PORT = process.env.PORT || 8800;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get("/",(req,res)=>{
+    res.json("Server working fine")
+})
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>JUSTICE HUB</div>
+  )
+}
+
+export default App
